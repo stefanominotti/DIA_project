9c3d6a1f7a0c8978bbf600411894ed9eb87d6d57
Stefano Minotti <stefanominotti1997@icloud.com>
2021-05-17 17:54:08 +0200

    feat: update bid learner

--- BidLearner.py
+++ BidLearner.py
@@ -12,7 +12,7 @@ class BidLearner(object):
         self.rounds_per_arm = np.zeros(len(self.arms))
         self.rewards_per_arm = [[] for _ in range(len(self.arms))]
         self.means = np.zeros(len(self.arms))
-        self.sigmas = np.ones(len(self.arms)) * 1e3
+        self.sigmas = np.ones(len(self.arms)) * 1e6
 
     def update_observations(self, arm_idx, reward):
         self.t += 1
@@ -22,8 +22,9 @@ class BidLearner(object):
         self.rounds_per_arm[arm_idx] += 1
 
     def pull_arm(self):
-        if self.t < 10:
-            return self.arms[np.random.choice(np.argwhere(self.means == 0).reshape(-1))]
+        if len(np.argwhere(self.rounds_per_arm == 0)) != 0:
+            return self.arms[np.random.choice(np.argwhere(self.rounds_per_arm == 0).reshape(-1))]
+
         samples = np.random.normal(self.means, self.sigmas)
         threshold_probs = np.array([norm(loc=loc, scale=scale).cdf(0) for (loc, scale) in zip(self.means, self.sigmas)])
         valid_arms_idx = np.argwhere(threshold_probs < self.negative_probability_threshold).reshape(-1)
@@ -38,7 +39,6 @@ class BidLearner(object):
         n_samples = len(self.rewards_per_arm[arm_idx])
         if n_samples > 1:
             self.sigmas[arm_idx] = np.std(self.rewards_per_arm[arm_idx]) / n_samples
-        print(self.means)
 
     def get_optimal_arm(self):
         return self.arms[np.argmax(self.means)]
