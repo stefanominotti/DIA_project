775d3d853fe35a2e054d1fe8ec70ff5c8ad22236
Stefano Minotti <stefanominotti1997@icloud.com>
2021-05-18 12:55:20 +0200

    feat: update bids experiment

--- experiment_bids.py
+++ experiment_bids.py
@@ -1,15 +1,16 @@
+from ObjectiveFunction import ObjectiveFunction
 from Environment import Environment
 from Scenario import Scenario
 from PriceUCBLearner import PriceUCBLearner
 from PriceTSLearner import PriceTSLearner
 from ReturnsEstimator import ReturnsEstimator
 from PriceGreedyContextGenerator import PriceGreedyContextGenerator
-from BidLearner import BidLearner
+from BidGPTSLearner import BidGPTSLearner
 import numpy as np
 import matplotlib.pyplot as plt
 
 scen = Scenario('scenario_example')
-price = 90
+price = 3
 prices = [price, price, price]
 
 print(f'Price {price}')
@@ -21,11 +22,12 @@ for b in scen.bids:
     cpc = np.array([customer_class.cost_per_click(b, noise=False)*customer_class.daily_clicks(b, noise=False) for customer_class in scen.customer_classes]).sum() / np.array([customer_class.daily_clicks(b, noise=False) for customer_class in scen.customer_classes]).sum()
     daily_clicks = np.array([customer_class.daily_clicks(b, noise=False) for customer_class in scen.customer_classes]).sum()
     true_rewards.append(daily_clicks * (conversion_rate * price * (1 + returns) - cpc))
-    print(daily_clicks * (conversion_rate * price * (1 + returns)), daily_clicks * cpc)
 optimal = true_rewards[np.argmax(true_rewards)]
 optimal_bid = scen.bids[np.argmax(true_rewards)]
 
-print(true_rewards)
+objective_function = ObjectiveFunction(scenario=scen, prices=[price])
+optimal, _, optimal_bid = objective_function.get_optimal_price_bid()
+
 print(optimal, optimal_bid)
 
 n_exp = 1
@@ -34,7 +36,7 @@ reward_per_experiment = [[] for _ in range(n_exp)]
 for exp in range(n_exp):
     print(f'Exp: {exp+1}')
     env = Environment(scen)
-    learner = BidLearner(scen.bids, 0.2)
+    learner = BidGPTSLearner(scen.bids, 0.2)
     customers_per_day = []
     bids_per_day = []
 
@@ -47,20 +49,18 @@ for exp in range(n_exp):
         bids_per_day.append(bid)
         reward = 0
 
+        print(bid)
+
         if day > 30:
             delayed_customers = customers_per_day.pop(0)
             bid = bids_per_day.pop(0)
             reward = 0
-            for customer_class in scen.customer_classes:
-                class_customers = list(filter(lambda customer: customer.customer_class == customer_class, delayed_customers))
-                class_converted_customers = list(filter(lambda customer: customer.conversion == 1, class_customers))
-                reward += sum(list(map(lambda customer: customer.conversion_price * (1 + customer.returns_count), class_converted_customers)))
-                reward -= sum(list(map(lambda customer: customer.cost_per_click, class_customers)))
+            converted_customers = list(filter(lambda customer: customer.conversion == 1, delayed_customers))
+            reward += sum(list(map(lambda customer: customer.conversion_price * (1 + customer.returns_count), converted_customers)))
+            reward -= sum(list(map(lambda customer: customer.cost_per_click, delayed_customers)))
             learner.update(bid, reward)
             reward_per_experiment[exp].append(reward)
-            print(learner.means)
-
-        print(learner.get_optimal_arm())
+            
     #print(returns_estimator.get_probabilities())
 
 
