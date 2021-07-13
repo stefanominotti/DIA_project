fix: objective function expected returns calculation

main/utils/ObjectiveFunction.py
@@ -8,7 +8,7 @@ class ObjectiveFunction(object):
         self.bids = bids if bids != None else self.scen.bids
 
     def get_optimal(self, price_discrimination=False):
-        returns_per_class = np.array([customer_class.returns_function.mean() for customer_class in self.scen.customer_classes])
+        returns_per_class = np.array([sum([x*(customer_class.returns_function.cdf(x+0.5) - customer_class.returns_function.cdf(x-0.5)) for x in range(self.scen.returns_horizon + 1)]) for customer_class in self.scen.customer_classes])
         optimal_price_per_bid = []
         rewards_per_bid_per_class = []
         rewards_per_bid = []
@@ -45,41 +45,5 @@ class ObjectiveFunction(object):
             optimal_reward = [np.max(rewards_per_bid)]
 
         return optimal_reward, optimal_price, optimal_bid
-
-    def get_optimal_discrimination(self):
-        rewards_per_prices = []
-        returns_values = np.array([sum([x*(customer_class.returns_function.cdf(x+0.5) - customer_class.returns_function.cdf(x-0.5)) for x in range(self.scen.returns_horizon + 1)]) for customer_class in self.scen.customer_classes])
-        for p in self.prices:
-            rewards_per_prices.append(np.array([customer_class.conversion(p, discrete=False) * p for customer_class in self.scen.customer_classes]))
-        rewards_per_prices = np.concatenate(list(map(lambda x: np.transpose([x]), rewards_per_prices)), axis=1)
-        prices_rewards = np.max(rewards_per_prices, axis=1)
-        optimal_prices =  np.take(self.prices, np.argmax(rewards_per_prices, axis=1))
-        total_rewards = []
-        for b in self.bids:
-            daily_clicks = np.array([customer_class.daily_clicks(b, noise=False) for customer_class in self.scen.customer_classes])
-            cpc = np.array([customer_class.cost_per_click(b, noise=False) for customer_class in self.scen.customer_classes])
-            total_rewards.append((prices_rewards*(1 + returns_values)-cpc)*daily_clicks)
-        total_rewards = np.concatenate(list(map(lambda x: np.transpose([x]), total_rewards)), axis=1)
-        optimals = np.max(total_rewards, axis=1)
-        optimal_bids =  np.take(self.bids, np.argmax(total_rewards, axis=1))
-        return optimals, optimal_prices, optimal_bids
-
-    def get_optimal_no_discrimination(self):
-        total_rewards = []
-        best_bids = []
-        returns_values = np.array([sum([x*(customer_class.returns_function.cdf(x+0.5) - customer_class.returns_function.cdf(x-0.5)) for x in range(self.scen.returns_horizon + 1)]) for customer_class in self.scen.customer_classes])
-        for p in self.prices:
-            price_reward = np.array([customer_class.conversion(p, discrete=False) * p for customer_class in self.scen.customer_classes])
-            bid_rewards = []
-            for b in self.bids:
-                daily_clicks = np.array([customer_class.daily_clicks(b, noise=False) for customer_class in self.scen.customer_classes])
-                cpc = np.array([customer_class.cost_per_click(b, noise=False) for customer_class in self.scen.customer_classes])
-                bid_rewards.append(((price_reward * (1 + returns_values) - cpc)*daily_clicks).sum())
-            total_rewards.append(np.max(bid_rewards))
-            best_bids.append(self.bids[np.argmax(bid_rewards)])
-        optimal = np.max(total_rewards)
-        optimal_price = self.prices[np.argmax(total_rewards)]
-        optimal_bid =  best_bids[np.argmax(total_rewards)]
-        return optimal, optimal_price, optimal_bid
 
 